e035e2d2eae7e1b94a4f0a5d2453ca6ee1222f52
Rishi <reshivenaganti@gmail.com>
2025-05-22 20:53:58 +0530

    updated home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,30 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './Navbar.css';
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-left">
+//         <div className="icon-circle">icon</div>
+//       </div>
+
+//       <div className="navbar-center">
+//         <div className="logo-text">MEAL - BRIDGE</div>
+//       </div>
+
+//       <div className="navbar-links">
+//         <Link to="/about">About</Link>
+//         <Link to="/donor">Donor</Link>
+//         <Link to="/donations">Donations</Link>
+//         <Link to="/admin">Admin</Link>
+//         <Link to="/help">Help</Link>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -6,12 +33,11 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <div className="icon-circle">icon</div>
-        <div className="logo-text">
-          <div className="logo-line">FOOD</div>
-          <div className="logo-line">COMPANION</div>
-        </div>
+        <div className="icon-circle">MB</div>
       </div>
+
+      <div className="brand-name">Meal-Bridge</div>
+
       <div className="navbar-links">
         <Link to="/about">About</Link>
         <Link to="/donor">Donor</Link>
